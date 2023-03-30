9736ae7046b3104e08d54e5f4ee8ce925e48ec7e
cfabiani <cesar.fabiani@sciencespo.fr>
2023-03-30 08:39:31 +0000

    OKK

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -630,6 +630,9 @@ class Graph:
             pathfinal = pathdest[1:]
             powerfinal = powerpathdest[1:]
 
+        if powerfinal == []:
+            return [], 0
+
         return pathfinal, max(powerfinal)
 
     """
@@ -773,29 +776,55 @@ def camion_par_routes(dict_route, dict_camion, tree):
 
     profondeurs, parents = tree.find_parents(1)
 
+    check = 0
+
     for trajet in dict_route_complete:
+        check = check + 1
+        print(check)
         src, dest, gain = dict_route[trajet]
         # On cherche le min_power du trajet
         inutile, p = tree.min_power_opti(src, dest, profondeurs, parents)
         dict_route_complete[trajet].append(p)
         # On trouve les camions qui peuvent effectuer le trajet
-        camions_possibles = []
+        #camions_possibles = []
+        """
         for camion in dict_camion:
+            indice_camion = indice_camion + 1
             puissance = dict_camion[camion][0]
-            cout = dict_camion[camion][1]
+            #cout = dict_camion[camion][1]
             if puissance >= p:
-                camions_possibles.append(dict_camion[camion])
+                a = dict_camion[camion].copy()
+                #a.append(indice_camion)
+                camions_possibles.append(a)
+                print(camions_possibles)
         # On trouve celui au coût minimal
         cout_minimal = min([i[1] for i in camions_possibles])
         indice = [i[1] for i in camions_possibles].index(cout_minimal)
         meilleur_camion = camions_possibles[indice]
         dict_route_complete[trajet].append(meilleur_camion[1])
+        """
+        indice_camion = 0
+        meilleur_camion = 0
+        coutmin = dict_camion[1][1]
 
+        for camion in dict_camion:
+            indice_camion += 1
+            puissance = dict_camion[camion][0]
+            if puissance >= p:
+                if dict_camion[camion][1] <= coutmin:
+                    coutmin = dict_camion[camion][1]
+                    meilleur_camion = indice_camion
+                    print(meilleur_camion)
+
+        dict_route_complete[trajet].append(meilleur_camion)
+                
     return dict_route_complete
 
-h = graph_from_file("input/network.1.in")
+"""
+h = graph_from_file("input/network.6.in")
 h_mst = h.kruskal()
 r = routes_from_file("input/routes.1.in")
-t = trucks_from_file("input/trucks.1.in")
+t = trucks_from_file("input/trucks.2.in")
 
 print(camion_par_routes(r, t, h_mst))
+"""
